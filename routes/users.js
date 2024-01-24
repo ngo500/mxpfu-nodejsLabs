f05ffaa230f3e3f3f6ac3c8af24c269a2d156284
ngo500 <72844093+ngo500@users.noreply.github.com>
2024-01-24 11:26:58 -0800

    Update users.js
    Added functionality to requests

--- routes/users.js
+++ routes/users.js
@@ -25,35 +25,63 @@ let users = [
 
 // GET request: Retrieve all users
 router.get("/",(req,res)=>{
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+    res.send(JSON.stringify({users},null,4));                           //send every user
 });
 
 // GET by specific ID request: Retrieve a single user with email ID
 router.get("/:email",(req,res)=>{
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  const email = req.params.email;                                       //store the specified email
+  let specific_users = users.filter(
+    (user) => user.email == email                                       //check which users have the email
+  );
+  res.send(specific_users)                                              //send the found users
 });
 
 
 // POST request: Create a new user
 router.post("/",(req,res)=>{
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  users.push({"firstName":req.query.firstName,                          //push the new user into list of users
+            "lastName":req.query.lastName,
+            "email":req.query.email,
+            "DOB":req.query.DOB});
+  res.send("Added new user " + (req.query.firstName) + "!");            //send complete message
 });
 
 
 // PUT request: Update the details of a user by email ID
 router.put("/:email", (req, res) => {
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
-});
+    const email = req.params.email;                                     //store given email
+    let filtered_users = users.filter((user) => user.email === email);  //filter user by email
+    if (filtered_users.length > 0) {                                    //if a user was found
+        let filtered_user = filtered_users[0];                          //pick the actual user
+        let DOB = req.query.DOB;                                        //store given DOB
+        let firstName = req.query.firstName;                            //store given firstName
+        let lastName = req.query.lastName;                              //store given lastName
+        if(DOB) {                                                       
+            filtered_user.DOB = DOB                                     //DOB has changed, change the it for user
+        }//if
+        if(firstName) {                                                 
+            filtered_user.firstName = firstName                         //firstName has changed, change it for user
+        }//if
+        if(lastName) {                                                  
+            filtered_user.lastName = lastName                           //lastName has changed, change it for user
+        }//if
+        
+        users = users.filter((user) => user.email != email);            //get rest of users by filtered email
+        users.push(filtered_user);                                      //put changed user back in
+        res.send(`User with the email ${email} updated.`);             //send update message confirmation
+    }
+    else{
+        res.send("Unable to find user!");                               //send error message
+    }
+  });
 
 
 // DELETE request: Delete a user by email ID
 router.delete("/:email", (req, res) => {
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
-});
+    const email = req.params.email;                                     //store the given email
+    users = users.filter((user) => user.email != email);                //filter users to not include given email
+    res.send(`User with the email ${email} deleted.`);                  //send confirmation message that user is gone
+  });
 
 module.exports=router;
